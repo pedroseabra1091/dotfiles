b80a2092b6db65fc13a0b8f855704af86c7f99c9
pedroseabra1091 <pedroseabra1091@gmail.com>
2024-12-17 14:39:48 +0000

    ASDF deprecation

--- Brewfile
+++ Brewfile
@@ -3,7 +3,6 @@ cask_args appdir: '/Applications'
 
 # tap 'homebrew/cask-fonts'
 
-brew 'asdf'
 brew 'zsh-autosuggestions'
 brew 'awscli'
 brew 'bash'
--- asdf/path.zsh
+++
@@ -1,6 +0,0 @@
-if uname -a | grep -q arm; then
-  . /opt/homebrew/opt/asdf/libexec/asdf.sh
-else
-  . /usr/local/opt/asdf/libexec/asdf.sh
-fi
-export PATH="~/.asdf/shims:$PATH"
